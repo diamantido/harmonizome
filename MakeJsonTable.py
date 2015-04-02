0978d7c7395827fefb57e115dfc871e4f11f642a
Andrew Rouillard <andrew.rouillard@mssm.edu>
2015-04-01 20:52:05 -0400

    parsed publication and download_date info and formatted into json structure

--- MakeJsonTable.py
+++ MakeJsonTable.py
@@ -16,10 +16,40 @@ with open('RESOURCE_META_DATA_20150331test.txt', 'r') as f:
 
 		linedict = {}
 
+		linedict['publication'] = []
+
 		# construct dict for each row of data
 		for i, entry in enumerate(entries):
 
-			linedict[headers[i]] = entry
+			if headers[i] == 'pmid':
+
+				pmids = entry.split(';')
+
+				for j, pmid in enumerate(pmids):
+
+					linedict['publication'].append({})
+
+					linedict['publication'][j]['pmid'] = pmid
+
+			elif headers[i] == 'citation':
+
+				citations = entry.split(';')
+
+				for j, citation in enumerate(citations):
+
+					linedict['publication'][j]['citation'] = citation
+
+					author = citation.split(',')[0].split('(')[0].rstrip() + ' et al.'
+
+					linedict['publication'][j]['author'] = author
+
+			elif headers[i] == 'download_date':
+
+				linedict[headers[i]] = {'year': entry[0:4], 'month': entry[4:6], 'day': entry[6:8]}
+
+			else:
+
+				linedict[headers[i]] = entry
 
 		metadata.append(linedict)
 
@@ -29,7 +59,7 @@ with open('RESOURCE_META_DATA_20150331test.txt', 'r') as f:
 	# print d
 
 
-with open('resource_table_20150331_test.json', 'w') as f:
+with open('resource_table_test.json', 'w') as f:
 
 	json.dump(metadata, f, indent=2)
 
