c45f8ac9a56e18578275cafb7aed9aef93c87b19
Greg <ggundersen@gmail.com>
2015-04-03 11:18:00 -0400

    New data and modified what table displays

--- js/view/Pages.js
+++ js/view/Pages.js
@@ -11,7 +11,7 @@ App.View.Pages = Backbone.View.extend({
     renderPage: function(page) {
         this.$el.empty();
         _.each(this.model.toJSON(), function(row) {
-            if (row.resource_name === page) {
+            if (row.directory === page) {
                 this.$el.append(App.renderTemplate('page', row));
             }
         }, this);
