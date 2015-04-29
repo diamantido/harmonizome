72e647c134d9a3e6149530697e6080f2aefed9b6
Greg <ggundersen@gmail.com>
2015-04-29 15:46:12 -0400

    Navigation, favicon, social links

--- server.js
+++ server.js
@@ -1,3 +1,18 @@
-var connect = require('connect');
-var serveStatic = require('serve-static');
-connect().use(serveStatic(__dirname)).listen(8080);
+/*var express = require('./lib/node_modules/express');
+
+var app = express();
+app.use('/static', express.static(__dirname + '/static'));
+app.use('/', express.static(__dirname + '/'));
+app.listen(8000);*/
+
+var express = require('./lib/node_modules/express');
+var php = require("./lib/node_modules/php"); 
+var path = require("./lib/node_modules/path"); 
+
+var app = express();
+
+console.log(php);
+
+//app.use("/", php.cgi("index.php")); 
+//app.listen(8000);
+//console.log("Server listening!");
