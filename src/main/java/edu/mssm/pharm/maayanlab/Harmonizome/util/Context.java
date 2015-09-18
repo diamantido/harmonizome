6f65640b19d145374bd5cf2dc50d97ece6e08e5c
Greg <ggundersen@gmail.com>
2015-09-18 17:30:54 -0400

    RC20150918: Hot fix for memory leak.

--- src/main/java/edu/mssm/pharm/maayanlab/Harmonizome/util/Context.java
+++ src/main/java/edu/mssm/pharm/maayanlab/Harmonizome/util/Context.java
@@ -1,5 +1,7 @@
 package edu.mssm.pharm.maayanlab.Harmonizome.util;
 
+import java.util.Set;
+
 import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
 import javax.servlet.annotation.WebListener;
@@ -8,13 +10,25 @@ import edu.mssm.pharm.maayanlab.common.database.HibernateUtil;
 
 @WebListener
 public class Context implements ServletContextListener {
-	
+
 	@Override
 	public void contextInitialized(ServletContextEvent event) {
-	}       
+	}
 
+	@SuppressWarnings("deprecation")
 	@Override
 	public void contextDestroyed(ServletContextEvent event) {
 		HibernateUtil.close();
+		// TODO: Find memory leak that requires server to be restarted after hot deploying several (3?) times.
+		Set<Thread> threadSet = Thread.getAllStackTraces().keySet();
+		for (Thread t : threadSet) {
+			if (t.getName().contains("Abandoned connection cleanup thread")) {
+				synchronized (t) {
+					System.out.println("Forcibly stopping thread to avoid memory leak: " + t.getName());
+					// Don't complain, it works.
+					t.stop(); 
+				}
+			}
+		}
 	}
 }
