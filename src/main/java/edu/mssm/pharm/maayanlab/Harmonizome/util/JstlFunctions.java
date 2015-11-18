d9c70085eb44b5e598c6aa6821897a8ca8b7bacf
Greg <ggundersen@gmail.com>
2015-11-17 20:12:52 -0500

    Amended JSTL util fn to handle multiple search terms

--- src/main/java/edu/mssm/pharm/maayanlab/Harmonizome/util/JstlFunctions.java
+++ src/main/java/edu/mssm/pharm/maayanlab/Harmonizome/util/JstlFunctions.java
@@ -2,9 +2,21 @@ package edu.mssm.pharm.maayanlab.Harmonizome.util;
 
 import org.apache.commons.lang3.StringUtils;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class JstlFunctions {
 
-    public static String highlightSearchTerm(String text, String term) {
+    public static String highlightSearchTerm(String text, String fullTerm) {
+        fullTerm = fullTerm.toLowerCase();
+        ArrayList<String> terms = new ArrayList<String>(Arrays.asList(fullTerm.split(" ")));
+        for (String t : terms) {
+            text = highlightSearchTerms(text, t);
+        }
+        return text;
+    }
+
+    private static String highlightSearchTerms(String text, String term) {
         term = term.toLowerCase();
         String capsTerm = StringUtils.capitalize(term);
 
