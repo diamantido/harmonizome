7cf7379a633e2f00e5671d28537aae2bc87a06a8
Daniel J. B. Clarke <u8sand@gmail.com>
2018-07-03 10:55:32 -0400

    Added Harmonizome-ML redirect

---
+++ src/main/java/edu/mssm/pharm/maayanlab/Harmonizome/page/HarmonizomeMLRedirect.java
@@ -0,0 +1,20 @@
+package edu.mssm.pharm.maayanlab.Harmonizome.page;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet(urlPatterns = { "/Harmonizome-ML", "/Harmonizome-ML/*" })
+public class HarmonizomeMLRedirect extends HttpServlet {
+
+	private static final long serialVersionUID = -4641583897568170335L;
+
+	@Override
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.sendRedirect("http://amp.pharm.mssm.edu/Harmonizome-ML");
+	}
+}
